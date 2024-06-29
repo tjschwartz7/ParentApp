865a4fbdcab80a36ca71f9a936f4f1920d1e7a26
tj <tj.schwartz2000@gmail.com>
2024-06-29 07:48:21 -0400

    Refactored the home tab into a settings tab. No design work yet.

--- app/src/main/java/com/example/baselineapp/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/baselineapp/ui/settings/SettingsViewModel.java
@@ -1,14 +1,14 @@
-package com.example.baselineapp.ui.home;
+package com.example.baselineapp.ui.settings;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class SettingsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public SettingsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
