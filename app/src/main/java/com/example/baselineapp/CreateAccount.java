9f1fdf44fc22d8ea9ae2b16076f71f372d87c9a2
tj <tj.schwartz2000@gmail.com>
2024-08-12 08:29:54 -0400

    Added the new profile tab without its design yet.

--- app/src/main/java/com/example/baselineapp/CreateAccount.java
+++ app/src/main/java/com/example/baselineapp/CreateAccount.java
@@ -32,6 +32,7 @@ public class CreateAccount extends AppCompatActivity {
                 finish();
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/baselineapp/MainActivity.java
+++ app/src/main/java/com/example/baselineapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.baselineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.baselineapp.ui.ProfileActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Button create_account_button = (Button) findViewById(R.id.id_profile);
+        create_account_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
