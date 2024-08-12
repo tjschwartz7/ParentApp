ba6f64fa243027f6faa4e4ea28d1e23c51317ec0
tj <tj.schwartz2000@gmail.com>
2024-08-12 08:05:50 -0400

    Finished the create account page. Added button and activity switching logic.

--- app/src/main/java/com/example/baselineapp/CreateAccount.java
+++ app/src/main/java/com/example/baselineapp/CreateAccount.java
@@ -1,6 +1,9 @@
 package com.example.baselineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -18,6 +21,14 @@ public class CreateAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
+        Button create_account_button = (Button) findViewById(R.id.b_create_account);
+        create_account_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccount.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
