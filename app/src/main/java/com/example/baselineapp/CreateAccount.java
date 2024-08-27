e3d6882b687d15e9d52c612d738cf7bc48884d4e
Bojacked <jacob.beamer@rockets.utoledo.edu>
2024-08-27 16:23:34 -0400

    Attempting to write to text files.

--- app/src/main/java/com/example/baselineapp/CreateAccount.java
+++ app/src/main/java/com/example/baselineapp/CreateAccount.java
@@ -3,6 +3,7 @@ package com.example.baselineapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
