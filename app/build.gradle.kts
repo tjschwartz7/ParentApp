079eb1c940066d3cdf81a2cbb30de38ede90fb6a
tj <tj.schwartz2000@gmail.com>
2024-09-04 14:00:58 -0400

    Added email code pending an email server.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,12 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.annotation)
     implementation(libs.activity)
+    // JavaMail API
+    implementation("com.sun.mail:android-mail:1.6.7")
+    implementation("com.sun.mail:android-activation:1.6.7")
+
+    // Optional: JavaBeans Activation Framework (JAF) (useful for attachments)
+    implementation("com.sun.activation:javax.activation:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
