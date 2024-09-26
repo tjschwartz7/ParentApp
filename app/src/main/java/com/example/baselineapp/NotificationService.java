6314de6e7d0ba5369738fa973216a74a7ee09623
tj <tj.schwartz2000@gmail.com>
2024-09-26 13:37:47 -0400

    Rebased dev onto TJ_3

--- app/src/main/java/com/example/baselineapp/NotificationService.java
+++ app/src/main/java/com/example/baselineapp/NotificationService.java
@@ -218,8 +218,8 @@ public class NotificationService extends Service {
                     if(tempWarning) msg += ("Temperature out of range - " +  String.valueOf(Globals.getTempVal())) + "\n";
                     if(bloodOxWarning) msg += ("Blood Oxygen out of range - " +  String.valueOf(Globals.getBloodOxVal())) + "\n";
 
-                    Globals.addNotification("WARNING", msg, getBaseContext().getFilesDir().getPath() + "/AccountData");
-                    //Globals.addNotification("WARNING", msg, this);
+                    //Globals.addNotification("WARNING", msg, getBaseContext().getFilesDir().getPath() + "/AccountData");
+                    Globals.addNotification("WARNING", msg, this);
                 }
                 //If this is still active during next tick, run notification
                 else warning_hasBeenActive5s = true;
@@ -249,8 +249,8 @@ public class NotificationService extends Service {
                     if(tempCaution) msg += ("Temperature out of range -  " +  String.valueOf(Globals.getTempVal())) + "\n";
                     if(bloodOxCaution) msg += ("Blood Oxygen out of range -  " +  String.valueOf(Globals.getBloodOxVal())) + "\n";
 
-                    Globals.addNotification("CAUTION", msg, getBaseContext().getFilesDir().getPath() + "/AccountData");
-                    //Globals.addNotification("CAUTION", msg, this);
+                    //Globals.addNotification("CAUTION", msg, getBaseContext().getFilesDir().getPath() + "/AccountData");
+                    Globals.addNotification("CAUTION", msg, this);
 
                 }
                 //If this is still active during next tick, run notification
